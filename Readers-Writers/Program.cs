ba645d7e39ca320fd7e5edc81b7d1d122a42fab3
franktominc <frankptominc@gmail.com>
2015-08-07 19:38:31 -0300

    First Commit

---
+++ Readers-Writers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Readers_Writers
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var t = new Thread(Run);
+
+            ThreadPool.SetMaxThreads(5,1);
+            var x = new Thread(Run);
+            x.Start();
+            t.Start();
+            Console.ReadKey();
+        }
+
+        private static void Run()
+        {
+          Console.WriteLine("Current Running on Thread {0}", Thread.CurrentThread.ManagedThreadId);
+
+        }
+    }
+
+}
