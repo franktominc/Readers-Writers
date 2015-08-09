86d8fa129af9380033183945ccb7578c023c5a88
franktominc <frankptominc@gmail.com>
2015-08-09 12:16:50 -0300

    Readers part working

---
+++ ConsoleApplication1/Teste.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readers_Writers {
+    class Teste {
+        public static void Main(string[] args)
+        {
+            FileStream fileStream = File.Open(@"C:\Users\frank\Desktop\Teste2.txt", FileMode.Open);
+
+            fileStream.Lock(0, Int64.MaxValue);
+            if()
+                Console.WriteLine("I Cant");
+
+            Console.ReadKey();
+        }
+    }
+}
--- Readers-Writers/Program.cs
+++ Readers-Writers/Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.IO.IsolatedStorage;
 using System.Threading;
 
 namespace Readers_Writers
 {
+
     internal class Program
     {
         private static void Main()
         {
-            var t = new Thread(Run);
-
-            ThreadPool.SetMaxThreads(5,1);
-            var x = new Thread(Run);
-            x.Start();
-            t.Start();
-            Console.ReadKey();
+            Supervisor sup = new Supervisor();
+            sup.process();
+            
+            Thread.Sleep(5000);
+           Console.WriteLine("Final value on sup is {0}", sup.Readers);
+           
+           Console.ReadKey();
         }
 
         private static void Run()
         {
-          Console.WriteLine("Current Running on Thread {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Current Running on Thread {0}", Thread.CurrentThread.ManagedThreadId);
 
         }
     }
-
 }
